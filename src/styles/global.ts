f2d25c3f75c647a5935108f01b9f811e0d3a16ee
Welber33 <jwwork21@gmail.com>
2024-02-07 11:18:35 -0300

    Github Blog - Setup do Projeto

---
+++ src/styles/global.ts
@@ -0,0 +1,55 @@
+import { createGlobalStyle } from "styled-components";
+import firaCode from "../assets/fonts/FiraCode-Regular.woff";
+
+export const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'FiraCode';
+    src: url(${firaCode}) format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    ::-webkit-scrollbar {
+      width: 0.45rem;
+    }
+
+    ::-webkit-scrollbar-track {
+      background: ${props => props.theme.colors["base-border"]}
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background: ${props => props.theme.colors["brand-blue"]};
+      border-radius: 999px;
+    }
+  }
+
+  body {
+    background: ${props => props.theme.colors["base-background"]};
+    color: ${props => props.theme.colors["base-text"]};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font: 400 ${props =>
+      props.theme.textSizes["text-text-m"]} 'Nunito', sans-serif;
+      line-height: 160%;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
